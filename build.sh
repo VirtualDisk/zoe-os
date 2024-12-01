e6d7ff5c5b08e04de9654ad788da12fbeab425cc
VirtualDisk <zoe@zoeblan.co>
2024-12-01 15:29:49 -0500

    add some fonts

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 update-ca-trust
+restorecon -vFr /usr/local/share/fonts/meslo
+fc-cache -v
 
 # this installs a package from fedora repos
 rpm-ostree install screen \
