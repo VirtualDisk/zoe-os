dd4eee2d4b9156760384337129170e00a689549e
VirtualDisk <zoe@zoeblan.co>
2024-12-18 20:00:53 -0500

    attempt to add dependencies

--- build.sh
+++ build.sh
@@ -19,6 +19,27 @@ fc-cache -v
 rpm-ostree install screen \
   podman-docker
 
+rpm-ostree install git \
+  wget \
+  jq \
+  cabextract \
+  vulkan-loader.x86_64 \
+  vulkan-loader.i686 \
+  pulseaudio-libs.x86_64 \
+  pulseaudio-libs.i686 \
+  freetype.x86_64 \
+  freetype.i686 \
+  libXcomposite.x86_64 \
+  libXcomposite.i686 \
+  libXrandr.x86_64 \
+  libXrandr.i686 \
+  libXfixes.x86_64 \
+  libXfixes.i686 \
+  libXcursor.x86_64 \
+  libXcursor.i686 \
+  libXi.x86_64 \
+  libXi.i686
+
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
